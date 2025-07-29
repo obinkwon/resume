04d7bb758de36d04a4d22f303258c95fd3690d6c
RAD권오빈 <e002993@ohpartner.co.kr>
2025-07-29 16:41:28 +0900

    mybatis 설정 추가

--- resume/resume-web/src/main/java/resume/web/LoginWebController.java
+++ resume/resume-web/src/main/java/resume/web/LoginWebController.java
@@ -3,19 +3,15 @@ package resume.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/web/login")
 public class LoginWebController {
 	
-    @GetMapping("/web/login")
+    @GetMapping("")
     public String loginView(final Model model) {
 
-        return "thymeleaf/login";
-    }
-    
-    @GetMapping("/web/main")
-    public String mainView(final Model model) {
-    	
-    	return "thymeleaf/main";
+        return "thymeleaf/oauth/login";
     }
 }
